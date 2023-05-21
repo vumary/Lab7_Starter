9d633da3d2677ed2f7fb3cd1a0f387fc8f9b6443
Eddie Li <eddieli.dev@gmail.com>
2023-05-21 15:02:08 -0700

    explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -34,25 +34,36 @@ async function init() {
  */
 function initializeServiceWorker() {
   // EXPLORE - START (All explore numbers start with B)
-  /*******************/
+  /****************/
   // ServiceWorkers have many uses, the most common of which is to manage
   // local caches, intercept network requests, and conditionally serve from
   // those local caches. This increases performance since users aren't
   // re-downloading the same resources every single page visit. This also allows
   // websites to have some (if not all) functionality offline! I highly
   // recommend reading up on ServiceWorkers on MDN before continuing.
-  /*******************/
+  /*****************/
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
+  if ('serviceWorker' in navigator) {
+    // B2. TODO - Listen for the 'load' event on the window object.
+    window.addEventListener('load', () => {
+      // Steps B3-B6 will be inside the event listener's function created in B2
+      // B3. TODO - Register './sw.js' as a service worker (The MDN article
+      // "Using Service Workers" will help you here)
+      navigator.serviceWorker.register('./sw.js')
+        .then((registration) => {
+          // B4. TODO - Once the service worker has been successfully registered, console
+          // log that it was successful.
+          console.log('ServiceWorker registration successful:', registration);
+        })
+        .catch((error) => {
+          // B5. TODO - In the event that the service worker registration fails, console
+          // log that it has failed.
+          console.log('ServiceWorker registration failed:', error);
+        });
+    });
+  }
   // STEPS B6 ONWARDS WILL BE IN /sw.js
 }
 
@@ -68,7 +79,7 @@ async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
-  if(localStorage.getItem("recipe") != null) {
+  if (localStorage.getItem("recipe") != null) {
     return JSON.parse(localStorage.getItem("recipes"));
   }
   /**************************/
@@ -104,29 +115,29 @@ async function getRecipes() {
     RECIPE_URLS.forEach((url) => fetchRecipe(url));
   });
 }
-  
-  // A4-A11 will all be *inside* the callback function we passed to the Promise
-  // we're returning
-  /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
-  // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
-  //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
-  //            you must either use "await fetch(...)" or "fetch.then(...)". This
-  //            function is using the async keyword so we recommend "await"
-  // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
-  //            NOTE: .json() is ALSO asynchronous, so you will need to use
-  //            "await" again
-  // A8. TODO - Add the new recipe to the recipes array
-  // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-  //            if you have, then save the recipes to storage using the function
-  //            we have provided. Then, pass the recipes array to the Promise's
-  //            resolve() method.
-  // A10. TODO - Log any errors from catch using console.error
-  // A11. TODO - Pass any errors to the Promise's reject() function
+
+// A4-A11 will all be *inside* the callback function we passed to the Promise
+// we're returning
+/**************************/
+// A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+//            declared above
+// A5. TODO - Since we are going to be dealing with asynchronous code, create
+//            a try / catch block. A6-A9 will be in the try portion, A10-A11
+//            will be in the catch portion.
+// A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
+//            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
+//            you must either use "await fetch(...)" or "fetch.then(...)". This
+//            function is using the async keyword so we recommend "await"
+// A7. TODO - For each fetch response, retrieve the JSON from it using .json().
+//            NOTE: .json() is ALSO asynchronous, so you will need to use
+//            "await" again
+// A8. TODO - Add the new recipe to the recipes array
+// A9. TODO - Check to see if you have finished retrieving all of the recipes,
+//            if you have, then save the recipes to storage using the function
+//            we have provided. Then, pass the recipes array to the Promise's
+//            resolve() method.
+// A10. TODO - Log any errors from catch using console.error
+// A11. TODO - Pass any errors to the Promise's reject() function
 
 /**
  * Takes in an array of recipes, converts it to a string, and then
@@ -153,3 +164,4 @@ function addRecipesToDocument(recipes) {
     main.append(recipeCard);
   });
 }
+
--- sw.js
+++ sw.js
@@ -1,15 +1,19 @@
 // sw.js - This file needs to be in the root of the directory to work,
-//         so do not move it next to the other scripts
+// so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
+
+const RECIPE_URLs = [
+  // Add the URLs of the recipes you want to cache here
+];
 
 // Installs the service worker. Feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   event.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
       // B6. TODO - Add all of the URLs from RECIPE_URLs here so that they are
-      //            added to the cache when the ServiceWorker is installed
-      return cache.addAll([]);
+      // added to the cache when the ServiceWorker is installed
+      return cache.addAll(RECIPE_URLs);
     })
   );
 });
@@ -23,18 +27,28 @@ self.addEventListener('activate', function (event) {
 self.addEventListener('fetch', function (event) {
   // We added some known URLs to the cache above, but tracking down every
   // subsequent network request URL and adding it manually would be very taxing.
-  // We will be adding all of the resources not specified in the intiial cache
+  // We will be adding all of the resources not specified in the initial cache
   // list to the cache as they come in.
-  /*******************************/
+  //
   // This article from Google will help with this portion. Before asking ANY
   // questions about this section, read this article.
   // NOTE: In the article's code REPLACE fetch(event.request.url) with
-  //       fetch(event.request)
+  // fetch(event.request)
   // https://developer.chrome.com/docs/workbox/caching-strategies-overview/
-  /*******************************/
+  //
   // B7. TODO - Respond to the event by opening the cache using the name we gave
-  //            above (CACHE_NAME)
-  // B8. TODO - If the request is in the cache, return with the cached version.
-  //            Otherwise fetch the resource, add it to the cache, and return
-  //            network response.
+  // above (CACHE_NAME)
+  event.respondWith(
+    caches.open(CACHE_NAME).then(function (cache) {
+      // B8. TODO - If the request is in the cache, return with the cached version.
+      // Otherwise fetch the resource, add it to the cache, and return
+      // the network response.
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function (networkResponse) {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
 });
